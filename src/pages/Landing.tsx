b9d58fd7ff84e1d7a3b84ac475556ad4bf2ab4f1
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-10-17 12:57:50 -0300

    2.0 | Landing page changes done

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -10,20 +10,26 @@ function Landing() {
     return (
         <div id="page-landing">
             <div className="content-wrapper">
-                <img src={logoImg} alt="Happy"/>
+                <div className="logo-location-contents">
+                    <img src={logoImg} alt="Happy"/>
+
+                    <div className="location">
+                        <strong>Guidoval</strong>
+                        <span>Minas Gerais</span>
+                    </div>
+                </div>
 
                 <main>
                     <h1>Leve felicidade para o mundo</h1>
                     <p>Visite orfanatos e mude o dia de muitas crianças.</p>
                 </main>
 
-                <div className="location">
-                    <strong>Guidoval</strong>
-                    <span>Minas Gerais</span>
-                </div>
-
                 <Link to="/app" className="enter-app">
                     <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+                </Link>
+
+                <Link to="" className="restricted-access-button">
+                    Acesso restrito
                 </Link>
             </div>
         </div>
