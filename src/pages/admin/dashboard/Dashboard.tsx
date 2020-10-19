d45acf76164e1491745a8f29f08584b3a009f634
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-10-19 20:59:20 -0300

    2.0 | Initializing RegisteredOrphanages styles

--- src/pages/admin/dashboard/Dashboard.tsx
+++ src/pages/admin/dashboard/Dashboard.tsx
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import SideDashboardBar from '../../../components/SideDashboardBar';
+import { FiMapPin, FiAlertCircle, FiPower } from 'react-icons/fi';
+import { useHistory } from 'react-router-dom';
 
-// import RegisteredOrphanages from './RegisteredOrphanages';
+import logoImg from '../../../images/map-marker.svg';
+
+import RegisteredOrphanages from './RegisteredOrphanages';
+
+import '../../../styles/pages/admin/dashboard/dashboard.css';
 
 export default function Dashboard() {
+    const history = useHistory();
+    const [selected, setSelected] = useState<'registered' | 'pending'>('registered');
+    const [alertCircle, setAlertCircle] = useState(false);
+
+    function handleLogoutButton() {
+        localStorage.clear();
+        sessionStorage.clear();
+
+        history.push('/');
+    }
+
     return (
-        <SideDashboardBar selected='registered' />
+        <div className="dashboard-container">
+            <div className="container">
+                <img src={logoImg} alt="Happy"/>
+
+                <div className="pages">
+                    <button
+                        type="button"
+                        className={
+                            selected === 'registered'
+                            ? 'registered-orphanages-page selected'
+                            : 'registered-orphanages-page'
+                    }>
+                        <FiMapPin size={24} />
+                    </button>
+
+                    <button
+                        type="button"
+                        className={
+                            selected === 'pending'
+                            ? 'pending-orphanages-page selected'
+                            : 'pending-orphanages-page'
+                        }
+                    >
+                        <div
+                            style={{ display: alertCircle ? 'block' : 'none' }}
+                            className="alert-circle"
+                        />
+                        <FiAlertCircle size={24} />
+                    </button>
+                </div>
+
+                <button onClick={handleLogoutButton} type="button" className="logout-button">
+                    <FiPower size={24} />
+                </button>
+            </div>
+            
+            <RegisteredOrphanages />
+        </div>
     );
 };
--- src/pages/admin/dashboard/RegisteredOrphanages.tsx
+++ src/pages/admin/dashboard/RegisteredOrphanages.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
+import '../../../styles/pages/admin/dashboard/registered-orphanages.css';
+
 export default function RegisterefOrphanages() {
-    return <h1>Orfanatos registrados</h1>;
+    return (
+        <div className="page-container">
+            <header>
+                <h1>Orfanatos cadastrados</h1>
+                <small>2 orfanatos</small>
+            </header>
+
+            <div className="line"></div>
+
+            <div className="orphanage">
+                
+            </div>
+        </div>
+    );
 };
