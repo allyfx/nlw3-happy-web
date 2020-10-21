21fcff58970f202c567081d0eb1b08056747dd5f
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-10-21 16:30:25 -0300

    2.0 | EditPendingOrphanage done

--- src/pages/admin/dashboard/EditPendingOrphanage.tsx
+++ src/pages/admin/dashboard/EditPendingOrphanage.tsx
@@ -1,9 +1,9 @@
-import React, { useState, FormEvent, ChangeEvent } from "react";
+import React, { useState, FormEvent, ChangeEvent, useEffect } from "react";
 import { Map, Marker, TileLayer } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
-import { FiPlus, FiCheck, FiXCircle } from "react-icons/fi";
+import { FiCheck, FiXCircle } from "react-icons/fi";
 
 import Sidebar from '../../../components/Sidebar';
 import mapIcon from '../../../utils/mapIcon';
@@ -11,17 +11,58 @@ import api from '../../../services/api';
 
 import '../../../styles/pages/admin/dashboard/edit-orphanage.css';
 
+interface OrphanageParams {
+  id: string;
+}
+
+interface Orphanage {
+  latitude: number;
+  longitude: number;
+  name: string;
+  about: string;
+  instructions: string;
+  opening_hours: string;
+  open_on_weekends: boolean;
+  images: Array<{
+    url: string;
+  }>
+}
+
 export default function EditOrphanage() {
   const history = useHistory();
+  const params = useParams<OrphanageParams>();
+
   const [position, setPosition] = useState({latitude: 0, longitude: 0});
+  const [orphanage, setOrphanage] = useState<Orphanage>();
 
   const [name, setName] = useState('');
   const [about, setAbout] = useState('');
   const [instructions, setInstructions] = useState('');
   const [opening_hours, setOpening_hours] = useState('');
   const [open_on_weekends, setOpen_on_weekends] = useState(true);
-  const [images, setImages] = useState<File[]>([]);
   const [previewImages, setPreviewImages] = useState<string[]>([]);
+
+  useEffect(() => {
+      api.get(`/orphanages/${params.id}`).then(response => {
+          const orph = response.data as Orphanage;
+          
+          setOrphanage(orph);
+          setPosition({ latitude: orph.latitude, longitude: orph.longitude });
+          setName(orph.name);
+          setAbout(orph.about);
+          setInstructions(orph.instructions);
+          setOpening_hours(orph.opening_hours);
+          setOpen_on_weekends(orph.open_on_weekends);
+
+          const images = orph.images.map(image => image.url);
+          
+          setPreviewImages(images);
+      });
+  }, [params.id]);
+
+  if (!orphanage) {
+    return <p>Carregando...</p>
+  }
 
   function handleMapClick(event: LeafletMouseEvent) {
     const { lat, lng } = event.latlng;
@@ -39,8 +80,6 @@ export default function EditOrphanage() {
 
     const selectedImages = Array.from(event.target.files);
 
-    setImages(selectedImages);
-
     const selectedImagesPreview = selectedImages.map(image => {
       return URL.createObjectURL(image);
     });
@@ -49,7 +88,31 @@ export default function EditOrphanage() {
   }
   
   async function handleSubmit(event: FormEvent) {
-    
+    event.preventDefault();
+
+    const { latitude, longitude } = position;
+
+    const data = new FormData();
+
+    data.append('id', params.id);
+    data.append('name', name);
+    data.append('about', about);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('instructions', instructions);
+    data.append('opening_hours', opening_hours);
+    data.append('open_on_weekends', String(open_on_weekends));
+
+    await api.post(`/orphanages/update`, data);
+    await api.post(`/admin/pending/${params.id}`);
+
+    history.push('/dashboard');
+  }
+
+  async function handleRefuseButton() {
+    await api.delete(`/orphanages/${params.id}`);
+
+    history.push('/dashboard');
   }
 
   return (
@@ -63,7 +126,7 @@ export default function EditOrphanage() {
 
             <div className="map-container">
               <Map 
-                center={[-21.152648,-42.7991747]} 
+                center={[orphanage.latitude, orphanage.longitude]} 
                 style={{ width: '100%', height: 280 }}
                 zoom={15}
                 onClick={handleMapClick}
@@ -72,13 +135,11 @@ export default function EditOrphanage() {
                   url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
 
-                { position.latitude !== 0 && (
-                  <Marker
+                <Marker
                     interactive={false}
                     icon={mapIcon}
                     position={[position.latitude, position.longitude]}
-                  />
-                )}
+                />
               </Map>
 
               <p>Clique no mapa para alterar a localização</p>
@@ -113,13 +174,13 @@ export default function EditOrphanage() {
               <div className="images-container">
                 {previewImages.map(image => {
                   return (
-                    <img key={image} src={image} alt={name} />
+                    <img key={image} src={image} alt={orphanage.name} />
                   );
                 })}
 
-                <label htmlFor="image[]" className="new-image">
+                {/* <label htmlFor="image[]" className="new-image">
                   <FiPlus size={24} color="#15b6d6" />
-                </label>
+                </label> */}
               </div>
 
               <input multiple onChange={handleSelectImages} type="file" id="image[]" />
@@ -170,7 +231,7 @@ export default function EditOrphanage() {
           </fieldset>
 
           <div className="buttons">
-            <button className="refuse-button" type="submit">
+            <button onClick={handleRefuseButton} className="refuse-button" type="button">
               <FiXCircle size={24} color="#FFF" /> Recusar
             </button>
 
